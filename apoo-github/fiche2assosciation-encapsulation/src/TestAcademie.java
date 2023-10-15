05c99375eb63939a0842fe6d166478cf5bbf4b5f
Kyllian Delbauche <kylliandelbauche@YUKY-MAC.local>
2023-10-15 11:23:39 +0200

    englober dans projet intelij

--- apoo-github/fiche2assosciation-encapsulation/src/TestAcademie.java
+++ apoo-github/fiche2assosciation-encapsulation/src/TestAcademie.java
@@ -1,3 +1,4 @@
+package src;
 
 public class TestAcademie {
 
@@ -12,23 +13,23 @@ public class TestAcademie {
 		Cours cours2 = new Cours("chant chorale", "1Q", professeur1);
 		Cours cours3 = new Cours("Piano", "3Q", professeur2);
 		Eleve eleve1 = new Eleve("Durand", "Jean", cours1, cours2);
-		Eleve eleve2 = new Eleve("Ponant","Sébastien",cours2,cours1);
+		Eleve eleve2 = new Eleve("Ponant","Sï¿½bastien",cours2,cours1);
 		System.out.println("Affichage des cours");
 		System.out.println("Cours 1 \n" + cours1 );
 		System.out.println("Cours 2 \n" + cours2 );
 		System.out.println("Cours 3 \n" + cours3 );
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("Affichage des élèves");
+		System.out.println("Affichage des ï¿½lï¿½ves");
 		System.out.println("Eleve 1 \n" + eleve1);
 		System.out.println("Eleve 2 \n" + eleve2);
 		System.out.println("----------------------------------------------------------------------------------");
 		eleve2.setCoursComplementaire(cours3);
-		System.out.println("Affichage des cours après changement");
+		System.out.println("Affichage des cours aprï¿½s changement");
 		System.out.println("Cours 1 \n" + cours1 );
 		System.out.println("Cours 2 \n" + cours2 );
 		System.out.println("Cours 3 \n" + cours3 );
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("Affichage des élèves");
+		System.out.println("Affichage des ï¿½lï¿½ves");
 		System.out.println("Eleve 1 \n" + eleve1);
 		System.out.println("Eleve 2 \n" + eleve2);
 
